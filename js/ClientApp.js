1d697772eaf7fcf678b91cbf3c050816f1f9cd67
christine hager <chhager@deloitte.com>
2017-01-17 16:24:41 -0800

    ends day 1

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { HashRouter, Match } from 'react-router'
+import { BrowserRouter, Match } from 'react-router'
 import Landing from './Landing'
 import Search from './Search'
 import '../public/normalize.css'
@@ -9,14 +9,14 @@ import '../public/style.css'
 const App = React.createClass({
   render () {
     return (
-      // this (hashrouter) will typically be your root component
-      <HashRouter>
+      // this (BrowserRouter) will typically be your root component
+      <BrowserRouter>
         <div className='app'>
           {/* homepage has to be exactly that pattern, otherwise you'll match literally every other route */}
           <Match exactly pattern='/' component={Landing} />
           <Match pattern='/search' component={Search} />
         </div>
-      </HashRouter>
+      </BrowserRouter>
     )
   }
 })
