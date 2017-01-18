98f0db6ed6cc5eed5ef2ceb00fb20c9141e9167a
christine hager <chhager@deloitte.com>
2017-01-18 11:18:51 -0800

    day

--- js/Search.js
+++ js/Search.js
@@ -1,22 +1,36 @@
 import React from 'react'
 import preload from '../public/data.json'
+import ShowCard from './ShowCard'
 
 const Search = React.createClass({
+  getInitialState () {
+    return {
+      searchTerm: ''
+    }
+  },
+  handleSearchTermChange (event) {
+    this.setState({searchTerm: event.target.value})
+  },
   render () {
     return (
       <div className='search'>
-        {preload.shows.map((show) => {
-          return (
-            <div className='show-card'>
-              <img src={`/public/img/posters/${show.poster}`} />
-              <div>
-                <h3>{show.title}</h3>
-                <h4>({show.year})</h4>
-                <p>{show.description}</p>
-              </div>
-            </div>
-          )
-        })}
+        <header>
+          <h1>chagersVid</h1>
+          <input onChange={this.handleSearchTermChange} value={this.state.searchTerm} type='text' placeholder='Search' />
+        </header>
+        <div>
+          {preload.shows
+            .filter((show) => {
+              return `${show.title} ${show.description}`.toUpperCase().indexOf(this.state.searchTerm.toUpperCase()) >= 0
+            })
+            .map((show) => {
+            // using elipses is the same as show={show} this example is es6
+              return (
+                <ShowCard key={show.imdbID} {...show} />
+              )
+            })
+          }
+        </div>
       </div>
     )
   }
